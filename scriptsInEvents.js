fa4a5edc12c2d640cae5473adfe2475eedd92002
chiragmummadi <85065237+chiragmummadi@users.noreply.github.com>
2022-02-10 21:20:31 +0530

    Add files via upload

---
+++ scriptsInEvents.js
@@ -0,0 +1,30 @@
+
+
+
+const scriptsInEvents = {
+
+		async GameSheet_Event7_Act5(runtime, localVars)
+		{
+			console.log("Tutorial");
+		},
+
+		async GameSheet_Event24_Act3(runtime, localVars)
+		{
+			console.log("enter")
+		},
+
+		async LevelSheet_Event3_Act1(runtime, localVars)
+		{
+			console.log("missing");
+		},
+
+		async LevelSheet_Event5_Act2(runtime, localVars)
+		{
+			console.log("find");
+			
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
